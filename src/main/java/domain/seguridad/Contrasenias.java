434f8d56f4676f35128bcdc9acb58aac373dbeff
JPAveni <javeni@frba.utn.edu.ar>
2022-04-19 18:29:14 -0300

    Agrego un poco de la validacion de contrasenia

--- src/main/java/domain/seguridad/Contrasenias.java
+++ src/main/java/domain/seguridad/Contrasenias.java
@@ -1,20 +1,66 @@
 package domain.seguridad;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Contrasenias {
     int caracteresMinimos=8;
-    public boolean esContraseniaValida(String contrasenia){
-        if(this.cantidadCaracteres(contrasenia)<caracteresMinimos || this.estaEnElTopPeoresContrasenias(contrasenia)) {
-            return false;
+    public boolean esContraseniaValida(String contrasenia) {
+        if (this.cumpleCondicionesDeContrasenia(contrasenia) && not(this.estaEnElTopPeoresContrasenias(contrasenia))) {
+            return true;
         }
-        return true;
+        return false;
+    }
+
+    private boolean cumpleCondicionesDeContrasenia(String contrasenia) {
+        return this.cantidadCaracteres(contrasenia)<caracteresMinimos && this.chequeoCaracteres(contrasenia);
+
+    }
+
+
+    private static boolean chequeoCaracteres(String contrasenia) {
+        char ch;
+        boolean capitalFlag = false;
+        boolean lowerCaseFlag = false;
+        boolean numberFlag = false;
+        for (int i = 0; i < contrasenia.length(); i++) {
+            ch = contrasenia.charAt(i);
+            if (Character.isDigit(ch)) {
+                numberFlag = true;
+            } else if (Character.isUpperCase(ch)) {
+                capitalFlag = true;
+            } else if (Character.isLowerCase(ch)) {
+                lowerCaseFlag = true;
+            }
+            if (numberFlag && capitalFlag && lowerCaseFlag)
+                return true;
+        }
+        return false;
     }
 
     private boolean estaEnElTopPeoresContrasenias(String contrasenia) {
         //TODO
         //leer del archivo y comparar c/u?
         return true;
+    }
+
+
+    public class ReadFile {
+        public static void main(String[] args) {
+            try {
+                File myObj = new File("peoresContrasenias.txt");
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    System.out.println(data);
+                }
+                myReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
     }
 
     public int cantidadCaracteres(String contrasenia){
