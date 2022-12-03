cb96bce3577c62b391cc5bf3e65df5889d3760a7
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2022-12-02 22:10:56 -0300

    actualizar dependencias pom y editar main de server

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -4,9 +4,25 @@ import spark.Spark;
 import spark.debug.DebugScreen;
 
 public class Server {
+//    public static void main(String[] args) {
+//        Spark.port(9000);
+//        Router.init();
+//        DebugScreen.enableDebugScreen();
+//    }
     public static void main(String[] args) {
-        Spark.port(9000);
+        Spark.port(getPort());
         Router.init();
         DebugScreen.enableDebugScreen();
     }
+
+    private static int getPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+
+        return 9000;
+    }
+
 }
